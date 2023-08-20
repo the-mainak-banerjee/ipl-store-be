1ee5ea98332995d651d903a0f0ef07402f50fd6d
Mainak <developer.mainakbanerjee@gmail.com>
2023-08-20 14:51:50 +0530

    feat: :sparkles: created schema for products

---
+++ models/productModel.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+const productSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A product must have a name'],
+    trim: true,
+  },
+  description: String,
+  price: {
+    type: Number,
+    required: [true, 'A product must have a price'],
+  },
+  team: {
+    type: String,
+    required: [true, 'A product must have a team'],
+    enum: ['csk', 'dc', 'gt', 'kkr', 'lsg', 'mi', 'pbks', 'rcb', 'rr', 'srh'],
+  },
+  category: {
+    type: String,
+    required: [true, 'A product must have a category'],
+    enum: ['jersy', 'cap', 'mask'],
+  },
+  rating: {
+    type: Number,
+    default: 3.5,
+    min: [1, 'Rating must be above 1.0'],
+    max: [5, 'Rating must be below 5.0'],
+  },
+  discount: Number,
+  src: {
+    type: String,
+    required: [true, 'A product must have a image src'],
+  },
+  featured: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+productSchema.virtual('slug').get(function () {
+  return this.name.split(' ').join('').toLowerCase();
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
