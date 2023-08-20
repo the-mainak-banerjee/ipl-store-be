963b68b96882bb2e1b5df4b08e8b9766482689d3
Mainak <developer.mainakbanerjee@gmail.com>
2023-08-20 17:12:57 +0530

    feat: :sparkles: added morgan package

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const productRoutes = require('./routes/productRoutes');
 
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use('/api/v1/products', productRoutes);
 
